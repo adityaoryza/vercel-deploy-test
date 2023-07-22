8efa694301aabcb09af0bf235d019db52feef441
adityaoryza <oryza429@gmail.com>
2023-07-22 21:46:35 +0700

    adding front-end url to backend

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [''],
+    origin: ['https://vercel-deploy-frontend.vercel.app'],
     methods: ['POST', 'GET'],
     credentials: true,
   })
